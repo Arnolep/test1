e1f54b007ec7d05efc1f84a922b277cbb1cf7174
Arnolep <arnolepee@gmail.com>
2022-10-07 09:45:20 +0200

    j

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-tabs/style/react-tabs.css';
 import './App.css';
+import {CAR_DATA, NUMBER_DATA, PERSON_DATA, PRODUCTS_DATA} from "./data/data";
+import {MenuCardPage} from "./pages/MenuCardPage";
+import {PicturesPage} from "./pages/PicturesPage";
+import {Tabs, TabList, Tab, TabPanel} from "react-tabs";
+import {NumbersPage} from "./pages/NumbersPage";
+import {CarsPage} from "./pages/CarsPage";
+import {PersonsPage} from "./pages/PersonsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Tabs>
+        <TabList>
+          <Tab>menu</Tab>
+          <Tab>pics</Tab>
+          <Tab>numbers</Tab>
+          <Tab>cars</Tab>
+          <Tab>persons</Tab>
+        </TabList>
+        <TabPanel>
+          <MenuCardPage products={PRODUCTS_DATA}/>
+        </TabPanel>
+        <TabPanel>
+          <PicturesPage/>
+        </TabPanel>
+        <TabPanel>
+          <NumbersPage numbers={NUMBER_DATA}/>
+        </TabPanel>
+        <TabPanel>
+          <CarsPage cars={CAR_DATA}/>
+        </TabPanel>
+        <TabPanel>
+          <PersonsPage persons={PERSON_DATA}/>
+        </TabPanel>
+      </Tabs>
   );
 }
 
